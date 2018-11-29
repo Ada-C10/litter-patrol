0d444f661f9fd0395be6f5b27dd594f1bb083664
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-11-28 18:23:29 -0800

    add icons to browser

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -5,18 +5,36 @@ import PropTypes from 'prop-types';
 
 class GameItem extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state= {
+      isSpotted: false,
+    };
+  }
+
+  onItemClick = () => {
+    // console.log(this.props.type);
+    this.setState({
+      isSpotted: true,
+    });
+  }
 
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
+    let itemClass = "game-item";
 
+    if (this.state.isSpotted) {
+      itemClass = "game-item spotted-litter"
+    }
+    // console.log(this.props.type);
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.type];
 
     return (
-      <div className="game-item" style={itemStyle}>
+      <div className={itemClass} style={itemStyle} onClick={ this.onItemClick }>
         <img src={icon} alt="Item" className="icon-item"></img>
       </div>
     );
@@ -27,6 +45,7 @@ class GameItem extends Component {
 GameItem.propTypes = {
   height: PropTypes.number.isRequired,
   layer: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
 }
 
 export default GameItem;
