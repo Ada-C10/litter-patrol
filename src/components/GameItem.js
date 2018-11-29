1dc0c7436c049c7e9ccb5f66dc699f114d67fe50
Ada <larmartinez2@gmail.com>
2018-11-29 14:02:09 -0800

    add correct icon

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,16 +4,33 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
+  //side question: can you have a constuctor without an inherante?
+  constructor(props) {
 
+    super(props);
+
+    // this.state = {
+    //   name: this.props.name,
+    //   type: this.props.type,
+    // };
+  }
 
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
+    const type = this.props.type
+    // console.log(type)
+
+
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+
+
+    //const icon = `ItemIcons.${type}` // does not evaluate
+    const icon =ItemIcons[type]
+    console.log(icon)
 
     return (
       <div className="game-item" style={itemStyle}>
