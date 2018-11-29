cc0144130d6e33104f13c216cd3b531963f51289
Semret Nicodimos <snicodimos@gmail.com>
2018-11-28 17:20:41 -0800

    added items to the game via the GameItem file.

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,10 +4,11 @@ import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
 class GameItem extends Component {
-  propTypes = {
+  static propTypes = {
     height: PropTypes.number.isRequired,
     layer: PropTypes.number.isRequired,
   }
+
 
   render() {
     const itemStyle = {
@@ -16,7 +17,7 @@ class GameItem extends Component {
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.type];
 
     return (
       <div className="game-item" style={itemStyle}>
