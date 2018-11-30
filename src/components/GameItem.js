1f3e5c37494bb2e009b6c0fd4c78887d73ffefbd
Ada <larmartinez2@gmail.com>
2018-11-29 22:33:21 -0800

    comments

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import style from '../App.css';
+import '../App.css';
 import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
 
@@ -15,20 +15,14 @@ class GameItem extends Component {
     };
   }
 
-  // it does not toggle, once it is changed, it stays changed - mark absent
-  // the change is: a green check, if the item has type 'litter', otherwise an X
-        // find css:
-          // * Assign this class when a non-litter item is clicked */
-          //.spotted-nature::before
-          // /* Assign this class when a litter item is clicked */
-          //.spotted-litter::before
-        // and increse score by one by calling function in onItemClicked in App because app contains score
 
   // item was clicked!
   onClick = () => {
     console.log(this)
     //
     if (this.state.type === "litter") {
+      // and increse score by one by calling function in onItemClicked in App because app contains score
+
       this.setState({ class: "spotted-litter" });
     } else {
       this.setState({ class: "spotted-nature" });
@@ -41,15 +35,14 @@ class GameItem extends Component {
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
+    // define icon
     const type = this.props.type
     const icon =ItemIcons[type]
+
+    // define class
     const originalClass = "game-item";
-
     const mark = this.state.class
-    //console.log(headerColor)
-    //<div className={this.state.class === "spotted-litter::before" || "spotted-nature::before"? header + " " + headerColor: header} style={itemStyle} >
 
-    //const mark = this.props.type === "litter" ? "spotted-litter::before" : "spotted-nature::before";
     return (
       <div className={originalClass + " " + mark } style={itemStyle} >
         <img src={icon} alt="Item" className="icon-item" onClick={ this.onClick }></img>
