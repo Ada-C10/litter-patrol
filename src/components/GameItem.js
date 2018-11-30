96ceb9c482f3f2587c2e68127bb205d2932ac187
Xtina <2006peacegypsy@gmail.com>
2018-11-29 19:27:25 -0800

    changed icons

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
-import '../App.css';
-import ItemIcons from '../ItemIcons.js';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import "../App.css";
+import ItemIcons from "../ItemIcons.js";
+import PropTypes from "prop-types";
 
 class GameItem extends Component {
-
-
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
-      zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
+      zIndex: this.props.layer // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
-
+    const icon = ItemIcons[this.props.type];
+    console.log(icon);
     return (
       <div className="game-item" style={itemStyle}>
-        <img src={icon} alt="Item" className="icon-item"></img>
+        <img src={icon} alt="Item" className="icon-item" />
       </div>
     );
   }
 }
 
-
 GameItem.propTypes = {
   height: PropTypes.number.isRequired,
-  layer: PropTypes.number.isRequired,
-}
+  layer: PropTypes.number.isRequired
+};
 
 export default GameItem;
