ffb8a45a5a0eafc0f57c267916d8803ef9a4acf1
Addison Cole <addison.cole85@gmail.com>
2018-11-29 20:31:04 -0800

    add prop to Game item to receive an item from items array, passes the type of that item, and is rendered by Game Item.

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -10,10 +10,11 @@ class GameItem extends Component {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
+
     };
 
     // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    const icon = ItemIcons[this.props.itemType]
 
     return (
       <div className="game-item" style={itemStyle}>
