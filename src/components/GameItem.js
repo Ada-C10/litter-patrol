63ca8b14f38f98a742c5ef31b19a3d591bc612ed
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-29 21:30:15 -0800

    checked and unchecked works as well as showing individual icons

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import '../App.css';
 import ItemIcons from '../ItemIcons.js';
 import PropTypes from 'prop-types';
+// import App from '../src/App.js'
+
 
 class GameItem extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+      divClass: "game-item"
+    }
+  }
+
+  changedToCheck = (event) => {
+    if (!this.state.checked){
+      this.setState({
+        checked: true,
+      });
+      if (this.props.itemType === "litter") {
+        this.setState({
+          divClass: "game-item spotted-litter"
+        })
+      }
+      else{
+        this.setState({
+          divClass: "game-item spotted-nature"
+        })
+      }
+    }
+  }
 
   render() {
     const itemStyle = {
@@ -12,11 +39,11 @@ class GameItem extends Component {
       zIndex: this.props.layer, // use props.layer to set z-index, so we display ontop of background
     };
 
-    // Update this to select the correct icon for each item
-    const icon = ItemIcons.rock;
+    // // Update this to select the correct icon for each item
+    const icon = ItemIcons[`${this.props.itemType}`];
 
     return (
-      <div className="game-item" style={itemStyle}>
+      <div className={this.state.divClass} style={itemStyle} id={this.props.index} onClick={this.changedToCheck}>
         <img src={icon} alt="Item" className="icon-item"></img>
       </div>
     );
