203280c7f8f26af3977be129e2229174d8abb431
Xtina <2006peacegypsy@gmail.com>
2018-11-29 22:02:52 -0800

    fixed onClick check and x

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -4,16 +4,48 @@ import ItemIcons from "../ItemIcons.js";
 import PropTypes from "prop-types";
 
 class GameItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+      divClass: "game-item"
+    };
+  }
+
+  changedToCheck = event => {
+    if (!this.state.checked) {
+      this.setState({
+        checked: true
+      });
+
+      if (this.props.type === "litter") {
+        this.setState({
+          divClass: "game-item spotted-litter"
+        });
+      } else {
+        this.setState({
+          divClass: "game-item spotted-nature"
+        });
+      }
+    }
+  };
+
   render() {
     const itemStyle = {
       bottom: `${this.props.height}px`, // use props.height to offset from the bottom of screen
       zIndex: this.props.layer // use props.layer to set z-index, so we display ontop of background
     };
 
-    const icon = ItemIcons[this.props.type];
+    const icon = ItemIcons[`${this.props.type}`];
     console.log(icon);
+
     return (
-      <div className="game-item" style={itemStyle}>
+      <div
+        className={this.state.divClass}
+        style={itemStyle}
+        id={this.props.index}
+        onClick={this.changedToCheck}
+      >
         <img src={icon} alt="Item" className="icon-item" />
       </div>
     );
